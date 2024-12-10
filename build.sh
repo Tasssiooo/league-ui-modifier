5f0aba00dabc9b961d9366b6d3d862a54b7ec338
Tasssiooo <tassioribeirofontes@proton.me>
2024-12-10 08:34:08 -0300

    Build script

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-pyinstaller --onefile --console \
+pyinstaller --onedir --clean --console \
   --icon "NONE" \
   --name league-ui-updater \
+  --contents-directory deps \
+  --add-data ./schemes:./schemes \
   src/main.py
