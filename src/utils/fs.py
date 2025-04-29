5c6a5ec3680a1f1e7bedb1be6b79fcd705a7d3b7
Tasssiooo <tassioribeirofontes@proton.me>
2025-04-29 02:13:08 -0300

    ♻️ refactor(fs): change get_mod_filename return type
    - modifies the return type of get_mod_filename from str to Path
    - this change provides more flexibility and functionality when working with file paths

--- src/utils/fs.py
+++ src/utils/fs.py
@@ -38,7 +38,7 @@ def copy_assets(src: Path, dst: Path) -> None:
     shutil.copytree(src, dst, dirs_exist_ok=True)
 
 
-def get_mod_filename() -> str:
+def get_mod_filename() -> Path:
 
     file = filedialog.askopenfilename(
         title="Select the file of the mod you want to update",
@@ -55,4 +55,4 @@ def get_mod_filename() -> str:
         )
         sys.exit(1)
 
-    return file
+    return Path(file)
