00938d53e28d9b64aaf365a7d1d7441f4e8ef9dd
Tasssiooo <tassioribeirofontes@proton.me>
2025-04-29 02:01:36 -0300

    ✨ feat(ritobin): download and save hashtables from communitydragon
    - add download and save hashtables from communitydragon to the deps/hashes directory
    - create deps/hashes directory if it doesn't exist

--- src/lib/ritobin.py
+++ src/lib/ritobin.py
@@ -7,6 +7,8 @@ from utils.fs import RELATIVE_PATH
 
 RITOBIN_CLI = RELATIVE_PATH / "deps" / "ritobin" / "ritobin_cli.exe"
 
+HASHES_DIR = RELATIVE_PATH / "deps" / "hashes"
+
 COMMUNITY_DRAGON_HASHTABLE = (
     "https://github.com/CommunityDragon/Data/raw/master/hashes/lol/hashes.{}.txt"
 )
@@ -16,26 +18,24 @@ def fetch_hashtables():
 
     print("Downloading hashtables...")
 
-    hashes_dir = RITOBIN_CLI.parent / "hashes"
-
-    if not hashes_dir.exists():
-        hashes_dir.mkdir()
+    if not HASHES_DIR.exists():
+        HASHES_DIR.mkdir()
 
     names = ["binentries", "binfields", "binhashes", "bintypes", "lcu", "rst", "game"]
 
     for name in names:
-        hash_file = hashes_dir / f"./hashes.{name}.txt"
+        hash_file = HASHES_DIR / f"./hashes.{name}.txt"
 
         if name == "game":
             for n in range(2):
-                hash_file = hashes_dir / "hashes.game.txt"
+                hash_file = HASHES_DIR / "hashes.game.txt"
 
                 if hash_file.exists():
                     hash_file.unlink()
 
                 r = requests.get(COMMUNITY_DRAGON_HASHTABLE.format(name) + f".{n}")
 
-                with open(hashes_dir / "hashes.game.txt", "a") as hashes_game_txt:
+                with open(HASHES_DIR / "hashes.game.txt", "a") as hashes_game_txt:
                     hashes_game_txt.write(r.content.decode("utf-8"))
             break
 
