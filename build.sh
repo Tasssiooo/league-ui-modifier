861c238fbe04f8900b3effeb32b69e91918ac803
Tasssiooo <tassioribeirofontes@proton.me>
2025-05-13 04:15:46 -0300

    📦 build(pyinstaller): improve build configuration
    - add missing paths for source files
    - include hidden imports for library compatibility

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 pyinstaller --onedir --clean --console --noconfirm \
+  --paths=src --paths=src/lib --paths=src/utils \
   --icon "icon.ico" \
   --name league-ui-updater \
   --contents-directory deps \
   --add-data schemes:schemes \
   --add-data src/lib/cslol_tools:cslol_tools \
   --add-data src/lib/ritobin:ritobin \
+  --hidden-import=lib \
+  --hidden-import=utils \
   --hidden-import=lib.cslol_tools \
   --hidden-import=lib.ritobin \
   --hidden-import=utils.configuration \
